0c3f3f88d27eb3f9e5ff0aa199b072981b462536
trunghieu2005-crypto <trunghieu1512005@gmail.com>
2026-03-16 16:03:10 +0700

    Delete Bai6_DATM/Bai6_DATM directory

--- Bai6_DATM/Bai6_DATM/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Bai6_DATM.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
